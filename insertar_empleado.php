f673088947389d8c656a9a876ff88bd4c46f248b
L3oEn <leonel.eng3640@alumnos.udg.mx>
2025-11-29 13:55:38 -0600

    Update insertar_empleado.php

--- insertar_empleado.php
+++ insertar_empleado.php
@@ -197,7 +197,7 @@ if (!isset($_SESSION['usuario']) || !isset($_SESSION['codigo'])) {
                         <i class="fas fa-save"></i>
                         <span>Guardar Empleado</span>
                     </button>
-                    <a href="menu.php" class="btn btn-secondary">
+                    <a href="menu_admin.php" class="btn btn-secondary">
                         <i class="fas fa-arrow-left"></i>
                         <span>Volver al Menú</span>
                     </a>
